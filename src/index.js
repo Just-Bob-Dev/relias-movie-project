9b6aec055d47af4eef6182308b79f33bea2c1434
Robert Hutchins <rjhutchins22@gmail.com>
2017-08-22 10:50:30 -0400

    Working on building a deployabe react app, deleted uneccessary react router functionality

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import ReactDOM from 'react-dom';
 //Styles which I don't think apply at the moment but I am leaving for now.
 import './styles/index.css';
 
-//React Router allows for other pages in React
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
 //Components needed for app
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './components/Home';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>,
+  <Home />,
   document.getElementById('root'));
 registerServiceWorker();
